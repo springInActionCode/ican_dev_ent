a3a7fd162b5cec5b1e9d0b7e5628f319b00dc661
zhoutianbao@e6yun.com <zhoutianbao@e6yun.com>
2020-09-30 00:36:25 +0800

    idea

--- install_idea.sh
+++ install_idea.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
  
 mkdir /usr/local/idea
-cd /usr/local/idea
 echo "Begin to install maven,Please waiting..."
 #下载解压 
 wget https://download.jetbrains.8686c.com/idea/ideaIC-2020.2.2.tar.gz
-tar -vxf  ideaIC-2020.2.2.tar.gz        
+tar -vxf  ideaIC-2020.2.2.tar.gz -C /usr/local/idea      
 
 echo "idea安装成功"
